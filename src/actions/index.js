e1f61114b2f2c6e4281ee83b700b6197dece693f
Michael McFarland <gardengrove422@gmail.com>
2018-03-19 13:02:29 -0400

    Axios API Fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,10 @@
 const API_KEY = '07585f5dd6fb0f3cd2c318456350b4b3';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+  return {
+    type: 'FETCH_WEATHER'
+  };
+}
